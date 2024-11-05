8ed52c60d2dc8c982df3d29fc89277052b351e69
msabeq94 <msabeq@outlook.com>
2024-11-05 14:50:58 +0200

    Commented out code related to creating an Azure AD user and generating a random password. Also commented out code related to creating an Azure consumption budget subscription.

--- main.tf
+++ main.tf
@@ -28,20 +28,20 @@ resource "azuread_group" "vf_core_subscription_owner" {
   members = try([azuread_user.create_subscription_owner.object_id],[])
   security_enabled = true
 }
-resource "azuread_user" "create_subscription_owner" {
-  display_name = "ashokaaa"
-  password = random_password.customer_subscription_owner_password.result
-  user_principal_name = "ashokaaa.maurya@spydertechuk.onmicrosoft.com"
-}
+# resource "azuread_user" "create_subscription_owner" {
+#   display_name = "ashokaaa"
+#   password = random_password.customer_subscription_owner_password.result
+#   user_principal_name = "ashokaaa.maurya@spydertechuk.onmicrosoft.com"
+# }
 
-resource "random_password" "customer_subscription_owner_password" {
-  length = 16
-  special = true
-  override_special = "!@#$"
-  lower = true
-  numeric = true
-  upper = true
-}
+# resource "random_password" "customer_subscription_owner_password" {
+#   length = 16
+#   special = true
+#   override_special = "!@#$"
+#   lower = true
+#   numeric = true
+#   upper = true
+# }
 
 output "customer_subscription_owner_username" {
   value = try(azuread_user.create_subscription_owner.object_id,"Customer Subscription Owner not defined")
@@ -67,31 +67,31 @@ locals {
 }
 
 #https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/consumption_budget_subscription
-resource "azurerm_consumption_budget_subscription" "vf_core_budget" {
-  name = "vf-core-budget-alert"
-  subscription_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  amount = var.budget_amount
-  time_grain = "Monthly"
-  time_period {
-    start_date = "${local.current_year_month}-01T00:00:00Z"
-    end_date = timeadd("${local.current_year_month}-01T00:00:00Z", "26280h")
-  }
-  notification{
-    enabled = true
-    operator = "GreaterThanOrEqualTo"
-    threshold = "75"
-    contact_emails = [var.customer_budget_contact_email]
-  }
-  notification{
-    enabled = true
-    operator = "GreaterThanOrEqualTo"
-    threshold = "95"
-    contact_emails = [var.customer_budget_contact_email]
-  }
-  notification{
-    enabled = true
-    operator = "GreaterThanOrEqualTo"
-    threshold = "99"
-    contact_emails = [var.customer_budget_contact_email]
-  }
-}
+# resource "azurerm_consumption_budget_subscription" "vf_core_budget" {
+#   name = "vf-core-budget-alert"
+#   subscription_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+#   amount = var.budget_amount
+#   time_grain = "Monthly"
+#   time_period {
+#     start_date = "${local.current_year_month}-01T00:00:00Z"
+#     end_date = timeadd("${local.current_year_month}-01T00:00:00Z", "26280h")
+#   }
+#   notification{
+#     enabled = true
+#     operator = "GreaterThanOrEqualTo"
+#     threshold = "75"
+#     contact_emails = [var.customer_budget_contact_email]
+#   }
+#   notification{
+#     enabled = true
+#     operator = "GreaterThanOrEqualTo"
+#     threshold = "95"
+#     contact_emails = [var.customer_budget_contact_email]
+#   }
+#   notification{
+#     enabled = true
+#     operator = "GreaterThanOrEqualTo"
+#     threshold = "99"
+#     contact_emails = [var.customer_budget_contact_email]
+#   }
+# }
