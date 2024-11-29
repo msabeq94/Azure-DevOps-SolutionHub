1c79da97e930dcf3a6398c17da0a0440cd46e52c
msabeq94 <msabeq@outlook.com>
2024-11-29 10:05:46 +0200

    Enhance remove_all.sh script to delete secrets and users with specific name filters

--- remove_all.sh
+++ remove_all.sh
@@ -1,20 +1,28 @@
-# Variable for your Key Vault name
+#!/bin/bash
 key_vault_name="vf-core-devops-pcr-kv"
 
-# Get the list of secrets
-secrets=$(az keyvault secret list --vault-name "$key_vault_name" --query "[].id" -o tsv)
+# Get the list of secrets with names starting with 'mohamed'
 
-# Loop through each secret and delete
+secrets=$(az keyvault secret list --vault-name "$key_vault_name" --query "[?starts_with(name, 'mohamed')].id" -o tsv)
+
+# Loop through each filtered secret and delete
+
+
 for secret in $secrets; do
     az keyvault secret delete --id "$secret"
+    echo "Deleted secret with ID: $secret "
 done
 
+
 # List all users whose display name starts with 'devops'
+
 users=$(az ad user list --query "[?starts_with(displayName, 'devops')].[objectId]" -o tsv)
 
 # Loop through the list and delete each user
+
 for user in $users; do
     az ad user delete --id $user
+    echo "Deleted user with object ID: $user"
 done
 
 
