87d60c8712271dfafcd78749fe318de319d69e36
Mohamed Sabeq <msabeq@outlook.com>
2025-01-01 16:08:51 +0200

    Add Azure provider credentials to Terraform configuration for improved authentication

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "2.87.0"
+      subscription_id = var.customer_subscription_id
+      client_id = var.customer_client_id
+      client_secret = var.customer_client_secret
+      tenant_id = var.customer_tenant_id
     }
   }
   backend "azurerm" {
